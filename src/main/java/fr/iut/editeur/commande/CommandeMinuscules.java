b53f3928d4c42ccfee9aad32f2de3de5f82a1a2c
Rayane Mallek <rayane.mallek@etu.umontpellier.fr>
2023-02-16 11:00:57 +0100

    Add minuscules

---
+++ src/main/java/fr/iut/editeur/commande/CommandeMinuscules.java
@@ -0,0 +1,28 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+public class CommandeMinuscules extends CommandeDocument {
+
+
+    public CommandeMinuscules(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 3) {
+            description();
+            return;
+        }
+
+        this.document.minuscules(Integer.parseInt(parameters[1]), Integer.parseInt(parameters[2]));
+        super.executer();
+    }
+
+    @Override
+    public void description() {
+        super.description();
+        System.out.println("majuscules;0;5");
+    }
+
+}
